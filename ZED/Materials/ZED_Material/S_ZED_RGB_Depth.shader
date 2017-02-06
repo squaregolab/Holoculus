1f5447d9cd8b1a1e04c1f136298c484b00e452ee
leroyjenkins66 <pierre.brosa@imerir.com>
2017-02-06 21:16:57 +0100

    Add files via upload

---
+++ ZED/Materials/ZED_Material/S_ZED_RGB_Depth.shader
@@ -0,0 +1,91 @@
+﻿Shader "Custom/ZED RGB-Depth"
+{
+	Properties
+	{
+		[HideInInspector] _MainTex("Base (RGB) Trans (A)", 2D) = "" {}
+	_DepthXYZTex("Depth texture", 2D) = "" {}
+	_CameraTex("Texture from ZED", 2D) = "" {}
+	
+	}
+	SubShader
+	{
+		// No culling or depth
+		ZWrite On
+		Tags{
+		"Queue" = "Background"
+		}
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+			#include "S_ZED_Utils.cginc"
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct Input {
+				float2 uv_MainTex;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+				
+			};
+
+			struct fragOut {
+
+				float depth : SV_Depth;
+				fixed4 color : SV_Target;
+			};
+			uniform float4x4 _ProjectionMatrix;
+			sampler2D _DepthXYZTex;
+			sampler2D _CameraTex;
+			uniform float4 _MaskTex_ST;
+			float4 _MainTex_ST;
+			uniform int _isGrey;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = mul(mul(mul(_ProjectionMatrix, UNITY_MATRIX_V), UNITY_MATRIX_M ), v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return o;
+			}
+
+
+			fragOut frag(v2f i)
+			{
+				//Get the depth in XYZ format
+				float3 colorXYZ = tex2D(_DepthXYZTex, (i.uv*fixed2(1, -1))).rgb;
+				//Compute the depth to work with Unity (1m real = 1m to Unity)
+				float depthReal = computeDepthXYZ(colorXYZ);
+
+				//Color from the camera
+				fragOut o;
+				if (_isGrey == 0) {
+					float3 colorCamera = tex2D(_CameraTex, i.uv*fixed2(1, -1)).bgr;
+					o.color.rgb = colorCamera.rgb;
+				}
+				else {
+					float colorCamera = tex2D(_CameraTex, i.uv*fixed2(1, -1)).a;
+					o.color.rgb = colorCamera;
+				}
+				o.color.a = 1;
+
+				o.depth = depthReal;
+
+
+				return o;
+			}
+			ENDCG
+		}
+	}
+}
---
+++ ZED/Scripts/ZEDManager.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System;
+
+public class ZEDManager : MonoBehaviour {
+    public sl.zed.ZEDCamera zedCamera;
+    private float[] pos;
+    private Matrix4x4 matrix;
+
+    public sl.zed.ZEDCamera.ZEDResolution_mode resolutionMode = sl.zed.ZEDCamera.ZEDResolution_mode.HD1080;
+    public float requestedFPS = 0.0f;
+    public sl.zed.ZEDCamera.MODE computingMode = sl.zed.ZEDCamera.MODE.PERFORMANCE;
+
+    public bool tracking = true;
+    public bool rendering = true;
+
+    // Use this for initialization
+    void Awake () {
+        zedCamera = sl.zed.ZEDCamera.GetInstance();
+        //Create a camera
+        zedCamera.CreateCamera(resolutionMode, requestedFPS);
+        Debug.Log("ZED SDK Version " + sl.zed.ZEDCamera.GetSDKVersion());
+
+        //Initialize the camera in performance mode.
+        sl.zed.ZEDCamera.ERRCODE e = zedCamera.Init(computingMode);
+        if (e != sl.zed.ZEDCamera.ERRCODE.SUCCESS)
+        {
+            throw new Exception("Initialization failed " + e.ToString());
+        }
+       
+        pos = IdentityMatrix();
+        if (this.tracking)
+        {
+            //Enables the tracking
+            if (!(this.tracking = zedCamera.EnableTracking(pos, false)))
+                throw new Exception("Error, tracking not available");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (zedCamera != null)
+        {
+            //Grab is needed to the tracking and the rendering
+            if (zedCamera.Grab(sl.zed.ZEDCamera.SENSING_MODE.FILL) == 0)
+            {
+                //Fill the different textures
+                if(rendering)
+                    zedCamera.Render();
+
+                if (this.tracking)
+                {
+                    zedCamera.GetPositionCamera(pos, sl.zed.ZEDCamera.MAT_TRACKING_TYPE.PATH);
+
+                    for (int i = 0; i < 4; ++i)
+                    {
+                        for (int j = 0; j < 4; ++j)
+                        {
+                            matrix[i, j] = pos[i * 4 + j];
+                        }
+                    }
+
+                    Vector4 v4 = matrix.GetColumn(3);
+                    Vector3 translate = new Vector3(v4.x, v4.y, v4.z);
+                    Quaternion rotation = sl.zed.ZEDCamera.Matrix4ToQuaternion(matrix);
+
+                    transform.localRotation = rotation;
+                    transform.localPosition = translate;
+                }
+            }
+        }
+    }
+
+
+    private float[] IdentityMatrix()
+    {
+        float[] pos = new float[16];
+
+        for (int i = 0; i < 16; ++i)
+        {
+            pos[i] = 0;
+        }
+        pos[0] = pos[5] = pos[10] = pos[15] = 1;
+        return pos;
+    }
+
+    void OnApplicationQuit()
+    {
+        if (zedCamera != null)
+            zedCamera.Destroy();
+        zedCamera = null;
+    }
+}
